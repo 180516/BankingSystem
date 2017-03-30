8e26d7647baf7abeca3b535028fe9c688d7fac83
DPP <dpeczek93@gmail.com>
2017-03-30 19:04:37 +0200

    Added project

---
+++ bankingsystem/src/main/java/pl/banking/BankingsystemApplication.java
@@ -0,0 +1,12 @@
+package pl.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingsystemApplication.class, args);
+	}
+}
