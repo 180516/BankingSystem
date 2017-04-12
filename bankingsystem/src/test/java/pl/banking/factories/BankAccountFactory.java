dc652953fb1033519648b16021d4fd8d3ef7a9e6
DPP <dpeczek93@gmail.com>
2017-04-12 17:53:21 +0200

    Started adding exceptions in bank account service

--- bankingsystem/src/test/java/pl/banking/factories/BankAccountFactory.java
+++ bankingsystem/src/test/java/pl/banking/factories/BankAccountFactory.java
@@ -1,14 +1,12 @@
 package pl.banking.factories;
 
-import pl.banking.service.wrappers.BankAccountWrapper;
-
 /**
  * Created by dpp on 4/10/17.
  */
 public class BankAccountFactory {
 
 
-//    public static Factory<BankAccountWrapper> makeBankAccountWrapper() {
+//    public static Factory<BankAccountDto> makeBankAccountWrapper() {
 //
 //    }
 
